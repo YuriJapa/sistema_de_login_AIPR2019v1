c69ceb0454205f078c8076a91a77486186f88f09
YuriJapa <49690709+YuriJapa@users.noreply.github.com>
2019-07-19 13:25:31 -0300

    Add files via upload
    Feito em aula 18/07/2019

---
+++ index.php
@@ -0,0 +1,56 @@
+<! DOCTYPE html>
+<html  lang = " en " >
+    <cabeça >
+        <meta  charset = " UTF-8 " >
+        <meta  name = " viewport "  content = " largura = largura do dispositivo, escala inicial = 1.0 " >
+        <Meta  http-equiv = " X-UA-Compatible "  conteúdo = " ie = bordo " >
+        <link  rel = " stylesheet "  href = " https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css " >
+        <link  rel = " stylesheet "  href = " https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css " >
+        <title > Sistema de login Japa Systems </ title >
+    </head >
+<body  class = " bg-dark " >
+    <main  class = " contêiner mt-4 " >
+      <! - Conteúdo Principal ->
+        <section  class = " row " >
+            <div  class = " col-lg-4 deslocamento-lg-4 "  id = " alerta " >
+                <div  class = " alerta alerta-sucesso centro de texto " >
+                <strong  id = " resultado " >
+                    Maravilhoso mudo sem o Sublime!
+                </strong >
+                </div >
+            </div >
+        </section >
+        
+    <! - Formulario de login ->
+            <section  class = " row " >
+            <div  class = " col-lg-4 deslocamento-lg-4 bg-luz arredondada "  id = " caixa login " >
+                <h2  classe = " texto-centro mt-2 " > Entrada no Sistema </ h2 >
+                <form  action = " # "  id = " formLogin "  classe = " p-2 " >
+                    <div  class = " form-group " >
+                        <input  type = " text "  nome = " nomeusuario "  id = " nomeusuario "  class = " forma-controle "  espaço reservado = " Nome de uso "  obrigatório >
+                    </div >
+                    <div  class = " form-group " >
+                        <input  type = " passoword "  nome = " senha do Usuario "  class = " forma-controle "  espaço reservado = " Senha do Usuario "  obrigatório >
+                    </div >
+                    <div  class = " form-group " >
+                    <div  class = " caixa de seleção personalizada de controle personalizado " >
+                        <input  type = " checkbox "  nome = " lembrar "  id = " lembrar "  class = " custom-ccontrol-input " >
+                    <label  for = " lembrar " >
+                    Lembrar de mim.
+                    </label >
+                    <A  href = " # "  id = " btnEsqueci "  class = " float-direita " >
+                    Esqueci a senha!
+                    </a >
+                    </div >
+                    </div >
+                    <div  class = " form-group " >
+                        <input  type = " submit "  value = " :: Enter :: "  nome = " btnEntrar "  id = " btnEntrar "  classe = " btn btn-primário btn-bloco " >
+                    </div >
+                    <div  class = " form-group " >
+                        <p  class = " cneter " > Novo usuário ?
+                        <A  href = " # "  id = " btnCadastrar " > Cadastre-se Aqui. </ A >
+                        </p >
+                    </div >
+             </form >
+         </div >
+    </section >
