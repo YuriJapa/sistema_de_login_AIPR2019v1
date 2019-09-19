bcc69e516a28af419629e3c58f4d1973294c2eca
YuriJapa <yuri_g_moraes@estudante.sc.senai.br>
2019-09-19 15:04:42 -0300

    Geração de Token
    1° passo para criar uma nova senha gerada

--- session.php
+++ session.php
@@ -3,6 +3,7 @@
 session_start();
 //Conectando com o banco de dados
 require_once 'configDB.php';
+
 if (isset($_SESSION['nomeUsuario'])) {
     //echo "usuário logado!";
     $usuario = $_SESSION['nomeUsuario'];
